04f440c0e58348dec2c0735e221ef3b8756bbfbf
Adriano Dias <adriano.rj.br@gmail.com>
2023-09-16 12:02:39 -0300

    Implements confirmation modal before deleting.

--- src/app/components/device-list/device-list.component.ts
+++ src/app/components/device-list/device-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { first } from 'rxjs';
 import { Device } from 'src/app/models/device';
 import { DeviceRequisitionService } from 'src/app/services/device-requisition.service';
+import { DeleteConfirmationComponent } from '../delete-confirmation/delete-confirmation.component';
 
 @Component({
   selector: 'app-device-list',
@@ -9,18 +10,31 @@ import { DeviceRequisitionService } from 'src/app/services/device-requisition.se
   styleUrls: ['./device-list.component.css'],
 })
 export class DeviceListComponent implements OnInit {
+  @ViewChild(DeleteConfirmationComponent)
+  deleteConfirmationComponent!: DeleteConfirmationComponent;
+  deleteConfirmed: boolean = false;
+  currentDevice!: Device;
+
+  confirmation($event: boolean) {
+    this.deleteConfirmed = $event;
+    if (this.deleteConfirmed) {
+      this.deviceRequisitionService
+        .deleteDevice(this.currentDevice)
+        .pipe(first())
+        .subscribe({
+          error: (err) => {
+            console.log(err);
+          },
+          complete: () => {
+            this.getDevices();
+          },
+        });
+    }
+  }
+
   public onDelete(device: Device): void {
-    this.deviceRequisitionService
-      .deleteDevice(device)
-      .pipe(first())
-      .subscribe({
-        error: (err) => {
-          console.log(err);
-        },
-        complete: () => {
-          this.getDevices();
-        },
-      });
+    this.currentDevice = device;
+    this.deleteConfirmationComponent.toggle();
   }
 
   devices?: Device[];
